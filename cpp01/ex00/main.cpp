e8b7d6887d0a0f77ff1ffdd61f7f20ee5fe86eee
marinitx <mhiguera@student.42madrid.com>
2025-03-22 19:02:50 +0100

    fixes

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -2,17 +2,18 @@
 
 int main()
 {
-    //create a zombie on the stack (no memory)
-    std::cout << INSTRUCTION << "*Creating a new zombie on the stack*" << RESET << std::endl;
+    std::cout << INSTRUCTION << "*Creating a zombie on the stack*" << RESET << std::endl;
     Zombie zombie1("Rauw Alejandro");
     zombie1.announce();
 
-    //create a zombie on the heap (allocating memory)
-    std::cout << INSTRUCTION << "*Creating a new zombie on the heap*" << RESET << std::endl;
+    std::cout << INSTRUCTION << "*Creating a zombie on the heap*" << RESET << std::endl;
     Zombie* zombie2;
     zombie2 = newZombie("jayco JhayCortez");
     zombie2->announce();
     delete(zombie2);
+
+    std::cout << INSTRUCTION << "Creating a zombie with randomChump" << RESET << std::endl;
+    randomChump("Bad Gyal");
 
     return 0;
 }
