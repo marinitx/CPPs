91c733d1c90f727fa2bde6a6822b901e0da1947c
marinitx <mhiguera@student.42madrid.com>
2025-04-03 17:21:10 +0200

    ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
-int replaceStr(std::string filename, std::string s1, std::string s2)
+int replaceStr(const std::string &filename, const std::string &s1, const std::string &s2)
 {
-    //open filename (check if its empty or chmod)
-    //find s1
+	if (s1.empty())
+	{
+		std::cerr << "Error: the string to search cannot be empty" << std::endl;
+		return 1;
+	}
+
+	if (s1 == s2)
+    {
+        std::cerr << "Error: the search string and replacement string are the same." << std::endl;
+        return 1;
+    }
+
+	std::ifstream inFile(filename);
+	if (!inFile)
+	{
+		std::cerr << "Error: could not open file " << filename << std::endl;
+		return 1;
+	}
+
+	std::string newFilename = filename + ".replace";
+	std::ofstream outFile(newFilename);
+
+	if (!outFile)
+	{
+		std::cerr << "Error: could not create file " << newFilename << std::endl;
+		return 1;
+	}
+
+	std::string line;
+	while (std::getline(inFile, line))
+	{
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+			pos += s2.length();
+		}
+		outFile << line << '\n';
+	}
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
-    {
-        std::cerr << "Error: Invalid number of arguments" << std::endl;
-        std::cerr << "Try this: <filename> <string to search> <string to replace>" << std::endl;
-        return 1;
-    }
+	if (argc != 4)
+	{
+		std::cerr << "Error: Invalid number of arguments" << std::endl;
+		std::cerr << "Try this: <filename> <string to search> <string to replace>" << std::endl;
+		return 1;
+	}
 
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 
-    return replaceStr(filename, s1, s2);
+	return replaceStr(filename, s1, s2);
 }
