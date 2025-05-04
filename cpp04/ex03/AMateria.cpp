11ad36536d877672a3a9dba06fdbad6fb2547a6f
marinitx <mhiguera@student.42madrid.com>
2025-05-04 18:44:34 +0200

    started ex03

---
+++ cpp04/ex03/AMateria.cpp
@@ -0,0 +1,16 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const & type) : _type(type) {}
+
+AMateria::~AMateria() {}
+
+std::string const & AMateria::getType() const
+{
+    return _type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+    (void)target;
+    //base class does nothing
+}
